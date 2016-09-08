a18ce7c736129456231da0c6393aafe0be7e8dcf
Matteo Menapace <hello@baddeo.com>
2016-09-08 19:18:09 +0100

    rename this

---
+++ RENAME_ME-config.js
@@ -0,0 +1,11 @@
+module.exports = 
+{
+  instagramKeys:
+  {
+    client_id: '...',
+    access_token: '...'
+  }
+}
+
+
+// Follow the steps at https://github.com/matteomenapace/daily_likesBot#stuff-you-need-from-instagram to get your Instagram client_id and access_token
